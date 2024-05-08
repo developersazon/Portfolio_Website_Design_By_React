82f625b0f2c00cf59d1366cc34c0ec56c971d55e
Sazon Mahmud <developer.sazon@gmail.com>
2024-05-08 17:55:57 +0600

    About page section is responsive for all device

--- src/Components/HomePage/About.js
+++ src/Components/HomePage/About.js
@@ -18,7 +18,7 @@ function About() {
         <MDBContainer className='my-5 mb-9'>
             <MDBRow className=''>
                 {/* Banner section start here */}
-                <MDBCol xs={6} md={6}>
+                <MDBCol xs={6} md={12} xl={6}>
                     <MDBCard className='mb-3'>
                         <MDBCardImage src={AboutImage} position='top' alt='...' />
                         <MDBCardBody>
@@ -29,7 +29,7 @@ function About() {
                 {/* Banner section end here */}
 
                 {/* About text section start here */}
-                <MDBCol xs={6} md={6} className='' style={{ textAlign: 'justify' }}>
+                <MDBCol xs={6} md={12} xl={6} className='' style={{ textAlign: 'justify' }}>
                         <h1 id='#aboutus' className='py-2 googlefont'>About Us</h1>
                         <p className='text-justify'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.<br/> 
                         It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
