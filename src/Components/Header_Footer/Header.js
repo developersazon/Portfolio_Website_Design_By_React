445717ad09c7558f0c83b1ef7161e5f1dbee0648
Sazon Mahmud <developer.sazon@gmail.com>
2024-05-13 14:48:23 +0600

    Added the router and link with all pages

--- src/Components/Header_Footer/Header.js
+++ src/Components/Header_Footer/Header.js
@@ -4,53 +4,68 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import ContactPage from '../ContactPage/Contact';
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Switch,
+  Route,
+  Link,
+  Router,
+} from "react-router-dom";
+
+
 
 export default function Header() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#">
-        <img
-              src='https://mdbootstrap.com/img/logo/mdb-transaprent-noshadows.webp'
-              height='30'
-              alt=''
-              loading='lazy'
-        />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="ms-auto px-5 my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="#">Home</Nav.Link>
-            <Nav.Link href="#">About</Nav.Link>
-            <Nav.Link href="#">Blogs</Nav.Link>
-            <Nav.Link href="#">Contact</Nav.Link>
-            <NavDropdown title="More" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#">Our Story</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#">
-                Mission
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#">
-                Vission
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-primary">Search</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+
+               <Navbar expand="lg" className="bg-body-tertiary">
+                <Container>
+                  <Navbar.Brand as={Link} to='/'>
+                  <img
+                        src='https://mdbootstrap.com/img/logo/mdb-transaprent-noshadows.webp'
+                        height='30'
+                        alt=''
+                        loading='lazy'
+                  />
+                  </Navbar.Brand>
+                  <Navbar.Toggle aria-controls="navbarScroll" />
+                  <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                      className="ms-auto px-5 my-2 my-lg-0"
+                      style={{ maxHeight: '100px' }}
+                      navbarScroll
+                    >
+                    
+                  <Nav.Link as={Link} to='/'>Home</Nav.Link>
+                  <Nav.Link as={Link} to='/about'>About</Nav.Link>
+                  <Nav.Link as={Link} to='/blogs'>Blogs</Nav.Link>
+                  <Nav.Link as={Link} to='/contact'>Contact</Nav.Link>
+                  <NavDropdown title="More" id="navbarScrollingDropdown">
+                    <NavDropdown.Item href="#">Our Story</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#">
+                      Mission
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#">
+                      Vission
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                    </Nav>
+                    <Form className="d-flex">
+                      <Form.Control
+                        type="search"
+                        placeholder="Search"
+                        className="me-2"
+                        aria-label="Search"
+                      />
+                      <Button variant="outline-primary">Search</Button>
+                    </Form>
+                  </Navbar.Collapse>
+                </Container>
+              </Navbar>
+
   );
 }
