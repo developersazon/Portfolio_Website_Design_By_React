98bfb22e2c0fe8c9cbc03b09ced0e67cdf252de6
Sazon Mahmud <developer.sazon@gmail.com>
2024-05-13 14:50:29 +0600

    Included all router and switch for properly work the webpage URL

--- src/index.js
+++ src/index.js
@@ -4,10 +4,43 @@ import './index.css';
 import App from './App';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Blog from './Components/HomePage/Blog';
+import About from './Components/HomePage/About';
+import Contact from './Components/ContactPage/Contact';
+import Home from './Components/HomePage/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home/>,
+      },
+      {
+        path: "about",
+        element: <About/>,
+      },
+      {
+        path: "blogs",
+        element: <Blog/>,
+        
+      },
+      {
+        path: "contact",
+        element: <Contact/>,
+        
+      },
+      
+    ],
+  }
+]);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+    <RouterProvider router={router} />
+  // </React.StrictMode>
 );
