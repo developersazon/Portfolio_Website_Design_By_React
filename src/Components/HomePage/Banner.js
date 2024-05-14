1c90cc0902d4f7a31378a29e33152eaacb69136e
Sazon Mahmud <developer.sazon@gmail.com>
2024-05-14 12:07:44 +0600

    Create the banner for all page

--- src/Components/HomePage/Banner.js
+++ src/Components/HomePage/Banner.js
@@ -1,17 +1,30 @@
 import React from 'react';
+import ReactPlayer from 'react-player/youtube';
 import { MDBCard, MDBCardBody, MDBCol, MDBContainer, MDBRow } from 'mdb-react-ui-kit';
 import BannerImage from '../img/Image1.jpg';
+import '../Css/style.css';
 
 
 
 export default function Banner() {
   return (
-
-        <MDBRow>
-             <MDBCol>
-                 <img src={BannerImage} alt='banner image' className='img-fluid h-50 w-100'></img>
-             </MDBCol>
-        </MDBRow>
-
+      <section className=''>
+                <MDBRow>
+                    <MDBCol>
+                            {/* Banner section start here */}
+                            <div id="intro" class="bg-image vh-100 shadow-1-strong">
+                                  <ReactPlayer className='videoStyle' src="https://mdbootstrap.com/img/video/Lines.mp4"/>
+                                  <div class="mask bgColor">  
+                                    <div class="container d-flex align-items-center justify-content-center h-100">
+                                      <div class="text-white shadow-2-strong p-4 rounded">
+                                           <h2 class="">Welcome to the page</h2>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                            {/* Banner section end here */}
+                    </MDBCol>
+                </MDBRow>
+      </section>
   );
 }
