19b28767c55037e4d166ba714ccea047be46a971
Sazon Mahmud <developer.sazon@gmail.com>
2024-05-14 13:40:34 +0600

    Added 2 two component which is used for contact page

--- src/Components/ContactPage/Contact.js
+++ src/Components/ContactPage/Contact.js
@@ -1,11 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import Banner from '../HomePage/Banner';
- 
+import ContactForm from './ContactForm';
+
 
 export default function Contact() {
   return (
     <div className="contact">
          <Banner/>
+         <ContactForm/>
+         <ContactForm/>
     </div>
   );
 }
