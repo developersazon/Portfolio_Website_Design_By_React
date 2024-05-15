8fd8e7593aacdf025e102b24ea07da6efc95adf4
Sazon Mahmud <developer.sazon@gmail.com>
2024-05-15 17:58:48 +0600

    This is the main about page where all the component ared included

--- src/Components/AboutPage/About-page.js
+++ src/Components/AboutPage/About-page.js
@@ -1,12 +1,17 @@
 import { Outlet } from 'react-router-dom';
 import Banner from '../HomePage/Banner';
 import About from './About';
+import Mission from './Mission';
+import Vission from './Vission';
+
 
 export default function AboutPage() {
   return (
     <div className="contact">
          <Banner/>
          <About/>
+         <Vission/>
+         <Mission/>
     </div>
   );
 }
