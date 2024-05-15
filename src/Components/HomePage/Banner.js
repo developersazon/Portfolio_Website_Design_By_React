c24a69cf8194934f278c96ee728a5078a1151a1d
Sazon Mahmud <developer.sazon@gmail.com>
2024-05-15 16:14:18 +0600

    Delete the old code and change the new design

--- src/Components/HomePage/Banner.js
+++ src/Components/HomePage/Banner.js
@@ -12,15 +12,10 @@ export default function Banner() {
                 <MDBRow>
                     <MDBCol>
                             {/* Banner section start here */}
-                            <div id="intro" class="bg-image vh-100 shadow-1-strong">
-                                  <ReactPlayer className='videoStyle' src="https://mdbootstrap.com/img/video/Lines.mp4"/>
-                                  <div class="mask bgColor">  
-                                    <div class="container d-flex align-items-center justify-content-center h-100">
-                                      <div class="text-white shadow-2-strong p-4 rounded">
-                                           <h2 class="">Welcome to our Website</h2>
+                            <div id="" class="App">
+                                      <div class="text-white">
+                                           <h2 class="h1 bgColor text-center googlefont bannerPadding">Welcome to our Website</h2>
                                       </div>
-                                    </div>
-                                  </div>
                                 </div>
                             {/* Banner section end here */}
                     </MDBCol>
